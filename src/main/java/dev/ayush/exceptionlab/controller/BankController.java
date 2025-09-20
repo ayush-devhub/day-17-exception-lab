23040aeec82179807e2ae55dffef0e3b41108348
ayush-devhub <ayush.devhub@gmail.com>
2025-09-20 09:41:01 +0530

    feat(controller): add BankController with hardcoded sample input

--- src/main/java/dev/ayush/exceptionlab/controller/BankController.java
+++ src/main/java/dev/ayush/exceptionlab/controller/BankController.java
@@ -1,4 +1,23 @@
 package dev.ayush.exceptionlab.controller;
 
+import dev.ayush.exceptionlab.service.BankService;
+
 public class BankController {
+    private final BankService bankService;
+
+    public BankController(BankService bankService) {
+        this.bankService = bankService;
+    }
+
+    public void simulate() {
+        try {
+            bankService.createAccount(1, 1000);
+            bankService.createAccount(2, 500);
+            bankService.transfer(1, 2, 200);
+            bankService.withDraw(2, 1000); // Will fail
+        } catch (Exception e) {
+            LoggerUtil.logException(e);
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
