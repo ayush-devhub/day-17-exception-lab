d59a408058af249df80d828ed8587b2aae9ea47e
ayush-devhub <ayush.devhub@gmail.com>
2025-09-20 09:41:46 +0530

    add(log): add LoggerUtil to log errors

--- src/main/java/dev/ayush/exceptionlab/util/LoggerUtil.java
+++ src/main/java/dev/ayush/exceptionlab/util/LoggerUtil.java
@@ -1,4 +1,27 @@
 package dev.ayush.exceptionlab.util;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+s
+
 public class LoggerUtil {
+    private static final Logger LOGGER = Logger.getLogger("BankLogger");
+
+    static {
+        try {
+            FileHandler handler = new FileHandler("logs/app.log", true);
+            handler.setFormatter(new SimpleFormatter());
+            LOGGER.addHandler(handler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void logException(Exception e) {
+        LOGGER.log(Level.SEVERE, e.getMessage(), e);
+    }
 }
