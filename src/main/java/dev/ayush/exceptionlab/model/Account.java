b5415c6b36006db0f7f72c1c5e94feaa4c3c0838
ayush-devhub <ayush.devhub@gmail.com>
2025-09-20 08:21:16 +0530

    feat(model): add Account model without input validation

--- src/main/java/dev/ayush/exceptionlab/model/Account.java
+++ src/main/java/dev/ayush/exceptionlab/model/Account.java
@@ -1,4 +1,28 @@
 package dev.ayush.exceptionlab.model;
 
 public class Account {
+    private final int id;
+    private double balance;
+
+    public Account(int id, double balance) {
+        this.id = id;
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return getId() + " " + getBalance();
+    }
 }
